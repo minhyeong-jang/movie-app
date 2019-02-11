b27390e7ed2f7aaefc27d19e37db2e7d27d21484
Noah <savedream75@gmail.com>
2019-02-11 15:28:54 +0900

    Add Movie js, css

---
+++ src/Movie.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './Movie.css';
+
+class Movie extends Component{
+  render(){
+    return(
+      <div>
+        <MoviePoster />
+        <h1>hello this is a movie</h1>
+      </div>
+    )
+  }
+}
+
+class MoviePoster extends Component{
+  render(){
+    return(
+      <img src="" />
+    )
+  }
+}
+
+export default Movie;
