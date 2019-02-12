afa0500f74fb212cf9357f1b38dccf1c5ffe6258
Noah <savedream75@gmail.com>
2019-02-12 13:51:46 +0900

    Updates

---
+++ precache-manifest.4b1f94564a603cfac86cc571efdc6be2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "b2b70dc561a8a4665cf0",
+    "url": "/movie_app/static/js/main.b2b70dc5.chunk.js"
+  },
+  {
+    "revision": "cf294e1d597418f7b338",
+    "url": "/movie_app/static/js/1.cf294e1d.chunk.js"
+  },
+  {
+    "revision": "b2b70dc561a8a4665cf0",
+    "url": "/movie_app/static/css/main.d3a40fdb.chunk.css"
+  },
+  {
+    "revision": "bf8cd5a46b58e01e51d3ceb5d719f450",
+    "url": "/movie_app/index.html"
+  }
+];
